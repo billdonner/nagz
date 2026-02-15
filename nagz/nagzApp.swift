f45e50e7a982c620ef6b228b61cb49195939412c
bill donner <billdonner@gmail.com>
2026-02-15 09:37:33 -0600

    Initial Commit

---
+++ nagz/nagzApp.swift
@@ -0,0 +1,17 @@
+//
+//  nagzApp.swift
+//  nagz
+//
+//  Created by bill donner on 2/15/26.
+//
+
+import SwiftUI
+
+@main
+struct nagzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
